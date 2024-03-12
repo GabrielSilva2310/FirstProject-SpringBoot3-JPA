ab05d282b23319946fb8109838547f5eaa3d5d3c
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-03-12 02:04:08 -0300

    Project created

---
+++ src/test/java/com/coursejava/StartSpringBoot/StartSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursejava.StartSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
